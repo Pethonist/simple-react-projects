cb905d15ef64193a7a0c9faa76efe73231ce799e
Pethonist <rostislavkpi@gmail.com>
2022-09-20 00:48:06 +0300

    Simple react build photo collection site

--- src/App.js
+++ src/App.js
@@ -1,65 +1,77 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Block } from './Block';
-import './index.scss';
+import Collection from './Collection';
+
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+];
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState('UAH');
-  const [toCurrency, setToCurrency] = useState('USD');
-  const [fromPrice, setFromPrice] = useState(0);
-  const [toPrice, setToPrice] = useState(1);
-
-  const ratesRef = useRef({});
+  const [collections, setCollections] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [categoryId, setCategoryId] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true);
+
+    const category = categoryId ? `category=${categoryId}` : '';
+
     axios
-      .get('https://cdn.cur.su/api/latest.json')
-      .then((response) => response.data)
-      .then((data) => {
-        ratesRef.current = data.rates;
-        onChangeToPrice(1);
-      })
+      .get(`http://localhost:3001/collections?_page=${page}&_limit=3&${category}`)
+      .then((response) => setCollections(response.data))
       .catch((error) => {
+        alert('Error getting collections');
         console.warn(error);
-        alert("Can't get current rates");
-      });
-  }, []);
-
-  const onChangeFromPrice = (value) => {
-    const price = value / ratesRef.current[fromCurrency];
-    const result = price * ratesRef.current[toCurrency];
-    setToPrice(result.toFixed(4));
-    setFromPrice(value);
-  };
-
-  const onChangeToPrice = (value) => {
-    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
-    setFromPrice(result.toFixed(4));
-    setToPrice(value);
-  };
-
-  useEffect(() => {
-    onChangeFromPrice(fromPrice);
-  }, [fromCurrency]);
-
-  useEffect(() => {
-    onChangeToPrice(toPrice);
-  }, [toCurrency]);
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
 
   return (
     <div className="App">
-      <Block
-        value={fromPrice}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrency}
-        onChangeValue={onChangeFromPrice}
-      />
-      <Block
-        value={toPrice}
-        currency={toCurrency}
-        onChangeCurrency={setToCurrency}
-        onChangeValue={onChangeToPrice}
-      />
+      <h1>My photo collection</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, index) => (
+            <li
+              key={obj.name}
+              className={categoryId === index ? 'active' : ''}
+              onClick={() => setCategoryId(index)}>
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          className="search-input"
+          placeholder="Search by name"
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+        />
+      </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          collections
+            .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((obj, index) => <Collection key={index} name={obj.name} images={obj.photos} />)
+        )}
+      </div>
+      <ul className="pagination">
+        {[...Array(5)].map((_, index) => (
+          <li
+            key={index}
+            className={page === index + 1 ? 'active' : ''}
+            onClick={() => setPage(index + 1)}>
+            {index + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
